bc1c809633d4e986386f336e82fecc435f8ecbbd
Dzianis Zhurau <den_fobos@mail.ru>
2025-05-01 00:13:53 +0300

    added: function to switching light, function save and load light state to EEPROM. Fixed: function changing light mode

--- src/main.cpp
+++ src/main.cpp
@@ -10,21 +10,23 @@ typedef struct
   uint8_t light_state;
   // Light mode can be cold white, warm white and mixed lamp turned on (1, 2, 3 respectively - like a byte [10000000], [01000000], [11000000])
   uint8_t light_mode;
+  uint32_t timestamp;
 } M_STATE;
 
 typedef struct
 {
-  uint8_t is_defined;          // levels of definition: 0 - not defined, 3 - well defined
+  uint8_t is_defined;          // levels of definition: 0 - not defined, 1 - defined
   uint8_t pin;                 // button pin on arduino
   char type;                   // 'L' - self locked (maintained)  button type, 'M' - momentary button type
   uint8_t front;               // 0 if pushed button connects to GND, 1 if connect to VCC
   uint8_t state;               // current_time state of button: 0 - off state, 1 - on state
-  uint8_t last_pin_state;      // previous state of button: 0 - off state, 1 - on state
+  uint8_t last_state;          // previous cycle button state
+  uint8_t last_pin_state;      // previous cycle state on button pin: 0 - off state, 1 - on state
   uint32_t current_state_time; // time when current state was changed to ON state
   uint32_t last_state_time;    // last time when state was changed to ON state
 } BUTTON;
 
-M_STATE light = {0, 3};
+M_STATE light = {0, 3, 0};
 BUTTON b1 = {
     0,
     12,
@@ -34,30 +36,31 @@ BUTTON b1 = {
     255,
     0,
     0};
-uint8_t prev_btn_state;
-uint32_t last_changed_btn_state_time;
 
 // put function declarations here:
 int digitalReadDebounce(int pin);
 void define_new_button(BUTTON *btn);
 int handle_press_button(BUTTON *btn);
+void save_m_state(M_STATE *id);
 void load_m_state(M_STATE *id);
 void load_button(BUTTON *btn, uint8_t btn_number);
-void handle_light(M_STATE *light);
-void change_light_mode(BUTTON *btn, M_STATE *light);
+void set_light_mode(M_STATE *light);
+void change_light_mode(M_STATE *light, BUTTON *btn);
+void handle_switching_light(M_STATE *id, BUTTON *btn);
 
 void setup()
 {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial)
+    ;
   pinMode(REL_1, OUTPUT);
   pinMode(REL_2, OUTPUT);
   pinMode(b1.pin, INPUT_PULLUP);
   digitalWrite(REL_1, HIGH);
   digitalWrite(REL_2, HIGH);
   define_new_button(&b1);
-  prev_btn_state = handle_press_button(&b1);
-  last_changed_btn_state_time = millis();
-  Serial.begin(9600);
+  load_m_state(&light);
 }
 
 void loop()
@@ -65,34 +68,22 @@ void loop()
   // put your main code here, to run repeatedly:
 
   handle_press_button(&b1);
-  change_light_mode(&b1, &light);
+  change_light_mode(&light, &b1);
+  handle_switching_light(&light, &b1);
 
   // debug messages
-  if (millis() % 200 == 0)
+  if (millis() % 100 == 0)
   {
     Serial.print("State: ");
     Serial.print(b1.state);
-    Serial.print(" Type: ");
-    Serial.print(b1.type);
-    Serial.print(" Cur_st_time: ");
-    Serial.print(b1.current_state_time / 1000);
-    Serial.print(" Lst_st_time: ");
-    Serial.print(b1.last_state_time / 1000);
-    Serial.print("\tLight_mode: ");
+    Serial.print(" Last state: ");
+    Serial.print(b1.last_state);
+    Serial.print(" Light_state: ");
+    Serial.print(light.light_state / 1000);
+    Serial.print(" Light_mode: ");
     Serial.println(light.light_mode);
   }
-  if (b1.state)
-  {
-    light.light_state = 1;
-    handle_light(&light);
-  }
-  else
-  {
-    light.light_state = 0;
-    handle_light(&light);
-  }
 }
-
 // put function definitions here:
 void define_new_button(BUTTON *btn)
 {
@@ -143,16 +134,11 @@ int handle_press_button(BUTTON *btn)
 
   if (btn->type == 'L')
   {
-    if (current_signal_state == btn->front)
+    if (current_signal_state != btn->last_pin_state)
     {
-      btn->state = 1;
+      // set button state dependig of button front
       btn->last_state_time = btn->current_state_time;
-      btn->current_state_time = current_time;
-    }
-    else
-    {
-      btn->state = 0;
-      btn->last_state_time = btn->current_state_time;
+      btn->state = current_signal_state == btn->front ? 1 : 0;
       btn->current_state_time = current_time;
     }
   }
@@ -166,15 +152,61 @@ int handle_press_button(BUTTON *btn)
       }
       else
       {
-        btn->state = !btn->state;
         btn->last_state_time = btn->current_state_time;
+        btn->state = !btn->state;
         btn->current_state_time = current_time;
       }
     }
-    btn->last_pin_state = current_signal_state;
   }
+  btn->last_pin_state = current_signal_state;
 
   return btn->state;
+}
+
+void handle_switching_light(M_STATE *id, BUTTON *btn)
+{
+  // if button has not been swiched last time skeep
+  static uint8_t last_btn_state;
+  if (btn->state == last_btn_state)
+  {
+    last_btn_state = btn->state;
+    return;
+  }
+
+  if (btn->state == 0)
+  {
+    id->light_state = 0;
+  }
+  else if (btn->state == 1)
+  {
+    id->light_state = 1;
+  }
+
+  if (id->light_state == 0)
+  {
+    digitalWrite(REL_1, HIGH);
+    digitalWrite(REL_2, HIGH);
+    last_btn_state = btn->state;
+    return;
+  }
+
+  if (id->light_mode == 1)
+  {
+    digitalWrite(REL_1, LOW);
+    digitalWrite(REL_2, HIGH);
+  }
+  else if (id->light_mode == 2)
+  {
+    digitalWrite(REL_1, HIGH);
+    digitalWrite(REL_2, LOW);
+  }
+  else if (id->light_mode == 3)
+  {
+    digitalWrite(REL_1, LOW);
+    digitalWrite(REL_2, LOW);
+  }
+  last_btn_state = btn->state;
+  return;
 }
 
 int digitalReadDebounce(int pin)
@@ -194,59 +226,73 @@ int digitalReadDebounce(int pin)
   return pin_state_accumulator / counter;
 }
 
-void handle_light(M_STATE *light)
-{
-  if (light->light_state == 1)
-  {
-    if (light->light_mode == 1)
-    {
-      digitalWrite(REL_1, LOW);
-      digitalWrite(REL_2, HIGH);
-    }
-    else if (light->light_mode == 2)
-    {
-      digitalWrite(REL_1, HIGH);
-      digitalWrite(REL_2, LOW);
-    }
-    else if (light->light_mode == 3)
-    {
-      digitalWrite(REL_1, LOW);
-      digitalWrite(REL_2, LOW);
-    }
-  }
-  else
-  {
-    digitalWrite(REL_1, HIGH);
-    digitalWrite(REL_2, HIGH);
-  }
-  // whire to EEPROM M_STATE ?
-}
+// void set_light_mode(M_STATE *light)
+// {
+//   if (light->light_state == 0)
+//   {
+//     digitalWrite(REL_1, HIGH);
+//     digitalWrite(REL_2, HIGH);
+//     return;
+//   }
 
-void change_light_mode(BUTTON *btn, M_STATE *light)
+//   if (light->light_mode == 1)
+//   {
+//     digitalWrite(REL_1, LOW);
+//     digitalWrite(REL_2, HIGH);
+//   }
+//   else if (light->light_mode == 2)
+//   {
+//     digitalWrite(REL_1, HIGH);
+//     digitalWrite(REL_2, LOW);
+//   }
+//   else if (light->light_mode == 3)
+//   {
+//     digitalWrite(REL_1, LOW);
+//     digitalWrite(REL_2, LOW);
+//   }
+//   return;
+// }
+
+void change_light_mode(M_STATE *light, BUTTON *btn)
 {
-  unsigned int pressing_interval_for_setting_ms = 1000;
+  unsigned int double_click_interval_ms = 200;
   int max_light_mode = 3; // it can be retrive from count of relays if they control same group of devices. For future development
-  uint32_t t2 = millis();
+  uint32_t t2 = btn->current_state_time;
   uint32_t t1 = btn->last_state_time;
-  if (t2 - t1 <= pressing_interval_for_setting_ms && btn->state == 1)
+  static uint8_t last_btn_state;
+  if (btn->state == last_btn_state)
+  {
+    last_btn_state = btn->state;
+    return;
+  }
+  if (t2 - t1 <= double_click_interval_ms)
   {
     light->light_mode = light->light_mode > 1 ? light->light_mode - 1 : max_light_mode;
+    // save_m_state(light);
   }
+  last_btn_state = btn->state;
+  return;
+}
+
+void save_m_state(M_STATE *id)
+{
+  int address_state = 0;
+  int address_mode = address_state + sizeof(id->light_state);
+  uint8_t first_byte = EEPROM.put(address_state, id->light_state);
+  uint8_t second_byte = EEPROM.put(address_mode, id->light_mode);
+  Serial.println(first_byte);
+  Serial.println(second_byte);
+  return;
 }
 
 void load_m_state(M_STATE *id)
 {
-  int address = 0;
-  EEPROM.put(address, *id);
-  if (id->light_state > 1)
-  {
-    id->light_state = 0;
-  }
-  if (id->light_mode != '0' || id->light_mode != '1' || id->light_mode != '2')
-  {
-    id->light_mode = '2';
-  }
-
+  int address_1 = 0;
+  int address_2 = address_1 + sizeof(id->light_state);
+  uint8_t first_byte = EEPROM.get(address_1, id->light_state);
+  uint8_t second_byte = EEPROM.get(address_2, id->light_mode);
+  Serial.println(first_byte);
+  Serial.println(second_byte);
   return;
 }
 
