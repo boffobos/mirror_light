f0824d9731cfa8b115663f46efc79ad2b682a5e6
Dzianis Zhurau <den_fobos@mail.ru>
2025-05-09 23:51:16 +0300

    added: handling incoming json and handling adding new buttons. Changed: logic of working some previous functions to adapt with new functionality

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,20 @@
 #include <ArduinoJson.h>
 
 // Todo:
-// - Timeout when light should turn off automaticaly
-// - Working with few buttons
-// Posibility to load pins number and working mode from external source then initialize it programmaticaly
-/* JSON example
+// + Timeout when light should turn off automaticaly
+// + Working with few buttons
+// +/- Posibility to load pins number and working mode from external source then initialize it programmaticaly
+//  Adaptive timeout (when after timeout event light is turned on in short period of time need to increase timeout, if not - decrease timeout)
+/* JSON example for buttons
   {
-    type: "B", ("R" - relay, "B" - button)
-    pin: 10,
+    "pin":10,
+    "device":"B",
+  }
+   JSON example for relay
+  {
+    "pin":"A1",
+    "device":"R",
+    "type":"L"
   }
 */
 // If button type M when timeout happens may should reset button state to 0
@@ -23,6 +30,11 @@
 #define REL_2 A1
 #define TIMEOUT 5L
 #define MAX_BUTTONS 5
+#define MAX_RELAYS 5
+#define START_BTN_PIN 2  // Define first GPIO in the row for buttons
+#define END_BTN_PIN 13   // Define last GPIO in the row for buttons
+#define START_REL_PIN A0 // Define first GPIO in the row for relays
+#define END_REL_PIN A7   // Define first GPIO in the row for relays
 
 // Type and struct definitions:
 struct M_STATE
@@ -40,17 +52,35 @@ struct BUTTON
   uint8_t pin;                 // button pin on arduino
   char type;                   // 'L' - self locked (maintained)  button type, 'M' - momentary button type
   uint8_t front;               // 0 if pushed button connects to GND, 1 if connect to VCC
-  uint8_t state;               // current_time state of button: 0 - off state, 1 - on state
+  uint8_t state;               // current state of button: 0 - off state, 1 - on state
   uint8_t last_state;          // previous cycle button state
   uint8_t last_pin_state;      // previous cycle state on button pin: 0 - off state, 1 - on state
   uint32_t current_state_time; // time when current state was changed to ON state
   uint32_t last_state_time;    // last time when state was changed to ON state
 };
 
+struct RELAY
+{
+  uint8_t pin;   // that is pin relay connected to
+  char type;     // 'L' - low state relay, 'H' - high state relay (triggered by LOW of HIGH signar respectively)
+  uint8_t state; // 0 - relay is turned off, 1 - turned on
+};
+
+struct PERIPHERALS
+{
+  struct BUTTON *button;
+  uint8_t is_button; // 1 - new button arriver, 0 - no
+  struct RELAY *relay;
+  uint8_t is_relay; // 1 - new relay arrived, 0 - no
+};
+
 // Global variables:
 
-BUTTON buttons[MAX_BUTTONS];
+struct BUTTON buttons[MAX_BUTTONS];
 uint8_t buttons_count = 0;
+
+struct RELAY relays[MAX_RELAYS];
+uint8_t relays_count = 0;
 
 M_STATE light = {0, 3, 0};
 // BUTTON b1 = {
@@ -86,6 +116,7 @@ void watching_buttons_state_changes(M_STATE *light, BUTTON btns[], int btn_count
 void change_light_mode(M_STATE *light, BUTTON *btn);
 void handle_switching_light(M_STATE *id);
 uint8_t read_input(char *buf, int len);
+PERIPHERALS handle_input(char *input);
 
 void setup()
 {
@@ -124,17 +155,49 @@ void loop()
 {
   // put your main code here, to run repeatedly:
   // BUTTON *arr[] = {&b1, &b2};
-  handle_press_button(&buttons[0]);
-  handle_press_button(&buttons[1]);
+  for (int i = 0; i < buttons_count; i++)
+  {
+    handle_press_button(&buttons[i]);
+  }
   watching_buttons_state_changes(&light, buttons, buttons_count);
   handle_switching_light(&light);
   uint8_t new_data = read_input(input_buffer, buf_len);
   if (new_data)
   {
-    deserializeJson(json, input_buffer);
-    const int pin = json["pin"];
-    Serial.println(input_buffer);
-    Serial.println(pin);
+    PERIPHERALS new_devices = handle_input(input_buffer);
+    Serial.println("New device: ");
+    Serial.print("is_button: ");
+    Serial.println(new_devices.is_button);
+    Serial.print("is_relay: ");
+    Serial.println(new_devices.is_relay);
+    if (new_devices.is_button)
+    {
+      define_new_button(new_devices.button);
+      int saved = button_state_rom(&buttons[buttons_count], buttons_count, 'S');
+      if (saved)
+        Serial.println("Button saved to ROM");
+      else
+        Serial.println("Button saving failed");
+      // buttons[buttons_count] = *new_devices.button;
+      buttons_count = buttons_count + 1;
+      Serial.print(" Buttons count: ");
+      Serial.println(buttons_count);
+      Serial.print("New button pin: ");
+      Serial.println(buttons[buttons_count - 1].pin);
+      Serial.print("New button def: ");
+      Serial.println(buttons[buttons_count - 1].is_defined);
+      Serial.print("New button type: ");
+      Serial.println(buttons[buttons_count - 1].type);
+      Serial.print("New button front: ");
+      Serial.println(buttons[buttons_count - 1].front);
+    }
+    else if (new_devices.is_relay)
+    {
+      Serial.println("Relay");
+    }
+    Serial.println("Finishing device definition");
+    // Serial.println(input_buffer);
+    // Serial.println(pin);
   }
   // debug messages
   // if (millis() % 100 == 0)
@@ -157,12 +220,21 @@ void loop()
 void define_new_button(BUTTON *btn)
 {
   // on the first power on this function should recognize button type: e.g. maintained or momentary, high or low level
+  pinMode(btn->pin, INPUT_PULLUP);
+
   unsigned long max_depress_time_ms = 300; // time need to depress and release momentary button
   int current_signal_state = digitalReadDebounce(btn->pin);
   unsigned long current_time = millis();
   unsigned long signal_changed_time = 0;
   int btn_prev_state = current_signal_state;
-  while (!btn->is_defined)
+
+  Serial.println("From define_new_button");
+  Serial.print("Button pin: ");
+  Serial.print(btn->pin);
+  Serial.print(" Button def: ");
+  Serial.println(btn->is_defined);
+
+  while (btn->is_defined != 1)
   {
     // need insert periodicaly changing pinMode between INPUT and INPUT_PULLUP to recognize LOW and HIGH buttons
     current_signal_state = digitalReadDebounce(btn->pin);
@@ -382,11 +454,18 @@ int button_state_rom(BUTTON *btn, uint8_t btn_number, char action)
   BUTTON backup;
   if (action == 'S')
   {
-    Serial.print("Saving....");
+    Serial.println("Saving....");
     if (!btn->is_defined)
-    {
       return 0;
-    }
+
+    if (btn->pin < START_BTN_PIN || btn->pin > END_BTN_PIN)
+      return 0;
+
+    if (btn->front != 0 && btn->front != 1)
+      return 0;
+
+    if (btn->type != 'L' && btn->type != 'M')
+      return 0;
 
     if (btn->pin != EEPROM.put(pin_offset, btn->pin))
     {
@@ -407,7 +486,7 @@ int button_state_rom(BUTTON *btn, uint8_t btn_number, char action)
     EEPROM.get(pin_offset, backup.pin);
     EEPROM.get(type_offset, backup.type);
     EEPROM.get(front_offset, backup.front);
-    if (backup.pin > 13 || backup.pin < 2)
+    if (backup.pin > END_BTN_PIN || backup.pin < START_BTN_PIN)
     {
       return 0;
     }
@@ -432,7 +511,7 @@ int button_state_rom(BUTTON *btn, uint8_t btn_number, char action)
 
 uint8_t read_input(char *buf, int len)
 {
-  // Function read data from serial to array and emits 0 if data still not finish and 1 when receiving data finish
+  // Function read data from serial to array and emits 0 if data still not finish or nothing and 1 when receiving data finish
   static uint8_t ndx = 0;
   static uint8_t receiving = 0;
   char starting_byte = '{'; // receiving only json formated string started from { and ending with \n
@@ -471,20 +550,74 @@ uint8_t read_input(char *buf, int len)
   return 0;
 }
 
-void handle_input(char *input)
+PERIPHERALS handle_input(char *input)
 {
-  uint8_t pin = 0;
-  signed char type;
+  // It is possible return pointer or struct it self. Whan is the best from performans POV?
+  PERIPHERALS dev;
   JsonDocument json;
+  uint8_t invalid_param = 127;
+  BUTTON *btn = &buttons[buttons_count];
+  RELAY *relay = &relays[relays_count];
 
+  Serial.println(input);
   DeserializationError error = deserializeJson(json, input);
-
-  if (!error)
+  Serial.println(error.c_str());
+  if (error)
   {
-    pin = json["pin"];
-    type = json["type"];
-    if (pin && type)
-    {
-    }
+    dev.is_button = 0;
+    dev.button = 0;
+    dev.is_relay = 0;
+    dev.relay = 0;
+    return dev;
   }
+
+  serializeJson(json, Serial);
+  int pin = json["pin"];
+  const char *device_type = json["device"];
+  Serial.print("Pin: ");
+  Serial.println(pin);
+  Serial.print("device_type: ");
+  Serial.println(device_type[0]);
+  if (device_type[0] == 'B')
+  {
+    dev.is_relay = 0;
+    dev.relay = 0;
+    btn->pin = (pin >= START_BTN_PIN ? (pin <= END_BTN_PIN ? pin : invalid_param) : invalid_param);
+    if (btn->pin == invalid_param)
+    {
+      dev.is_button = 0;
+      dev.button = 0;
+    }
+    btn->is_defined = 0;
+    dev.is_button = 1;
+    dev.button = btn;
+  }
+  else if (device_type[0] == 'R')
+  {
+    dev.is_button = 0;
+    dev.button = 0;
+
+    const char *rel_type = json["type"];
+    Serial.print("rel_type: ");
+    Serial.println(rel_type[0]);
+    relay->pin = (pin >= START_REL_PIN ? (pin <= END_REL_PIN ? pin : invalid_param) : invalid_param); // Check if pin in right range
+    relay->type = ((rel_type[0] == 'L') || (rel_type[0] == 'H') ? rel_type[0] : invalid_param);       // Check if json have only H of L for relay type
+    if (relay->pin == invalid_param || relay->type == invalid_param)
+    {
+      dev.is_relay = 0;
+      dev.relay = 0;
+      return dev;
+    }
+    dev.is_relay = 1;
+    dev.relay = relay;
+  }
+  else
+  {
+    dev.is_button = 0;
+    dev.button = 0;
+    dev.is_relay = 0;
+    dev.relay = 0;
+    return dev;
+  }
+  return dev;
 }
